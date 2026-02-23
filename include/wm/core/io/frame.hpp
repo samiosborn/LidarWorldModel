3d798b7f0595019f49680e1d5923610b6753ae40
Sami Osborn <samiosborn@live.co.uk>
2026-02-23 21:29:26 +0000

    do not redefine PointXYZI in frame.hpp

--- include/wm/core/io/frame.hpp
+++ include/wm/core/io/frame.hpp
@@ -1,20 +1,12 @@
 // File: include/wm/core/io/frame.hpp
 #pragma once
 
-#include <cstdint>
 #include <string>
 #include <vector>
 
-#include "wm/core/events/event_sink.hpp"  // TimestampNs
+#include "wm/core/types.hpp"  // TimestampNs, PointXYZI
 
 namespace wm {
-
-struct PointXYZI {
-  float x{0.f};
-  float y{0.f};
-  float z{0.f};
-  float intensity{0.f};
-};
 
 struct Frame {
   // Logical time for the frame. For synth: ticks since start. For replay: dataset time or ticks.
